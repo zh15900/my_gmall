f92147b6bc0510ea95c7aa0678f2df86e2ac4950
zh15900 <15960127619@163.com>
2022-02-16 01:03:01 +0800

    网关测试

---
+++ gulimall-gateway/src/main/java/com/zh/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.zh.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+//开启注册发现
+@EnableDiscoveryClient
+//排除数据库相关配置
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+
+}
